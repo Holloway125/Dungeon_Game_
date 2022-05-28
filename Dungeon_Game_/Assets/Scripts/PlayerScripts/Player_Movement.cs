9dbc6d78dc07d4dbe261b19461a4b77ca1c920cc
Boskazuru <mcmullendalton@yahoo.com>
2022-05-28 12:03:38 -0700

    PlayerMovement
    walking to the left is blurry cause i was having problems with editing software can fix later pretty easily especially sense the main character will not look like this

---
+++ Dungeon_Game_/Assets/Scripts/PlayerScripts/Player_Movement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
+    Vector2 movement;
+
+
+
+    void Update() //Called every frame
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+
+        
+    }
+
+
+    void FixedUpdate() //Called 50 times a sec
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+
+    }
+}
