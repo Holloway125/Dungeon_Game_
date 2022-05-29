78f69b922db8604a01ad9c21a2f4fe14fbadbcb5
Holloway125 <benny.mcsan@gmail.com>
2022-05-29 11:10:11 -0700

    stamina bar

--- Dungeon_Game_/Assets/Character/Player/Abilities/Dash.cs
+++ Dungeon_Game_/Assets/Character/Player/Abilities/Dash.cs
@@ -2,19 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
 [CreateAssetMenu]
+
+
 public class Dash : Ability
 {
     public float dashVelocity;
     public override void Activate(GameObject parent)
     {
+        PlayerResource stamina = parent.GetComponent<PlayerResource>();
         Player_Movement movement = parent.GetComponent<Player_Movement>();
-        movement.speed = dashVelocity;
+        if(stamina.Stamina.value >=20f)
+        {
+        movement.speed = movement.speed * dashVelocity;
+        stamina.Stamina.value -= 20f;
+        }
     }
 
     public override void BeginCooldown(GameObject parent)
     {
         Player_Movement movement = parent.GetComponent<Player_Movement>();
-        movement.speed = 5f;
+        movement.speed = 12f;
     }
 }    
