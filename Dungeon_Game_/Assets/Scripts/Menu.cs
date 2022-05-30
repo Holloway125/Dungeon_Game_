554b4ca5aed9b1c618dba0fda9224490a5285fc7
Holloway125 <benny.mcsan@gmail.com>
2022-05-30 09:21:09 -0700

    added keybinds and pause feature to char menus

--- Dungeon_Game_/Assets/Scripts/Menu.cs
+++ Dungeon_Game_/Assets/Scripts/Menu.cs
@@ -10,6 +10,11 @@ public static bool GameIsPaused = false;
 
 public GameObject pauseMenu;
 public GameObject settingsPanel;
+public GameObject invPanel;
+public GameObject charPanel;
+public GameObject talentPanel;
+public GameObject abilitiesPanel;
+public GameObject logPanel;
 
 void Update()
 {
@@ -20,7 +25,68 @@ void Update()
             Resume();
         }else
         {
-            Pause();
+            PauseMenu();
+        }
+    }
+
+        if(Input.GetKeyDown(KeyCode.Tab))
+    {
+        if (GameIsPaused)
+        {
+
+            Resume();
+            
+        }else
+        {
+            PauseInv();
+        }
+    }
+
+            if(Input.GetKeyDown(KeyCode.C))
+    {
+        if (GameIsPaused)
+        {
+
+            Resume();
+        }else
+        {
+            PauseChar();
+        }
+    }
+
+            if(Input.GetKeyDown(KeyCode.N))
+    {
+        if (GameIsPaused)
+        {
+
+            Resume();
+        }else
+        {
+            PauseTalent();
+        }
+    }
+
+            if(Input.GetKeyDown(KeyCode.P))
+    {
+        if (GameIsPaused)
+        {
+
+            Resume();
+        }else
+        {
+            PauseAbility();
+        }
+    }
+
+            if(Input.GetKeyDown(KeyCode.L))
+    {
+        if (GameIsPaused)
+        {
+
+            Resume();
+        }else
+        {
+            PauseLog();
         }
     }
 }
@@ -29,16 +95,62 @@ void Resume()
 {
     settingsPanel.SetActive(false);
     pauseMenu.SetActive(false);
+    invPanel.SetActive(false);
+    charPanel.SetActive(false);
+    talentPanel.SetActive(false);
+    abilitiesPanel.SetActive(false);
+    logPanel.SetActive(false);
     Time.timeScale = 1f;
     GameIsPaused = false;
 
 }
 
-void Pause()
+void PauseMenu()
 {
     
     pauseMenu.SetActive(true);
     Time.timeScale = 0f;
     GameIsPaused = true;
 }
+
+void PauseInv()
+{
+    
+    invPanel.SetActive(true);
+    Time.timeScale = 0f;
+    GameIsPaused = true;
+}
+
+void PauseChar()
+{
+    
+    charPanel.SetActive(true);
+    Time.timeScale = 0f;
+    GameIsPaused = true;
+}
+
+void PauseTalent()
+{
+    
+    talentPanel.SetActive(true);
+    Time.timeScale = 0f;
+    GameIsPaused = true;
+}
+
+void PauseAbility()
+{
+    
+    abilitiesPanel.SetActive(true);
+    Time.timeScale = 0f;
+    GameIsPaused = true;
+}
+
+void PauseLog()
+{
+    
+    logPanel.SetActive(true);
+    Time.timeScale = 0f;
+    GameIsPaused = true;
+}
+
 }
