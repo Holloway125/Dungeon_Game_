ee5eb22a7d0886250d6246e7e8d7f17eb7c8f385
Holloway125 <benny.mcsan@gmail.com>
2022-05-30 10:02:50 -0700

    prefabs changes

--- Dungeon_Game_/Assets/Scripts/playerAbilities/Ability.cs
+++ Dungeon_Game_/Assets/Scripts/playerAbilities/Ability.cs
@@ -9,8 +9,8 @@ public class Ability : ScriptableObject
     public float cooldownTime;
     public float activeTime;
     
-    
 
-    public virtual void Activate(GameObject parent){}
+
+    public virtual void Activate(GameObject player){}
     public virtual void BeginCooldown(GameObject parent){}
 }
