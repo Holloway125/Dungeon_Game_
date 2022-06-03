b9974063d703cf514a1022bede48d933fa752570
Boskazuru <mcmullendalton@yahoo.com>
2022-06-02 21:10:51 -0700

    Dalton

--- Dungeon_Game_/Assets/Scripts/playerAbilities/PlayerResource.cs
+++ Dungeon_Game_/Assets/Scripts/playerAbilities/PlayerResource.cs
@@ -5,9 +5,33 @@ using UnityEngine.UI;
 
 public class PlayerResource : MonoBehaviour
 {
-    public Slider Health;
+    public int maxHealth = 100;
+    public int currentHealth;
     public Slider Energy;
     public Slider Stamina;
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            TakeDamage(20);
+        }
+    }
+
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
+    }
 
     void FixedUpdate()
     {
