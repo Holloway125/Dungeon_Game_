835fe5b3c34a2a4be0928222467cc702f942156c
Holloway125 <benny.mcsan@gmail.com>
2022-07-10 09:51:29 -0700

    Monsters

---
+++ Dungeon_Game_/Assets/Scripts/Npc/npc_dialogue.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npc_dialogue : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public GameObject dialogText;
+    public string dialog;
+    public bool playerInRange;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerInRange)
+        {
+            if (dialogBox.activeInHierarchy)
+            {
+                dialogBox.SetActive(false);
+            }
+            else
+            {
+                dialogBox.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            dialogBox.SetActive(false);
+        }
+    }
+}
