657af18687d44ad6057972b519dbe90f71ad0708
Boskazuru <mcmullendalton@yahoo.com>
2022-07-10 12:28:30 -0700

    skilltree work

--- Dungeon_Game_/Assets/Scripts/SkillTree/SkillUnlocks.cs
+++ Dungeon_Game_/Assets/Scripts/SkillTree/SkillUnlocks.cs
@@ -1,31 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class SkillUnlocks : MonoBehaviour
 {
-    [SerializeField]
-    private int _skillCost;
-
     private LevelSystem lvlSystem;
     private GameObject _player;
+    int _skillCost;
+    int _skillMax;
+    int _skillMin;
+    //Add new skills here and move the button onto SkillHolder Scriptable Object
+    public List<Button> Skills;
 
     void Start()
-    {
+    {   
         _player = GameObject.FindWithTag("Player");
         lvlSystem = _player.GetComponent<LevelSystem>();
     }
 
     void UnlockSkill()
     {
+        _skillMin++;
         lvlSystem.SkillPoints = lvlSystem.SkillPoints - _skillCost;
     }
 
     public void Buy()
     {
-
-        if(lvlSystem.SkillPoints >= _skillCost)
+        if(lvlSystem.SkillPoints >= _skillCost && _skillMin != _skillMax)
         {
             UnlockSkill();
         }
