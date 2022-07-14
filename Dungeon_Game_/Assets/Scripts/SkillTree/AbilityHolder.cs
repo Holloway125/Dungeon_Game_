32499bc836282a58df18e31e0cbcdb9c3f5e411c
Boskazuru <mcmullendalton@yahoo.com>
2022-07-13 19:06:43 -0700

    SkillTree Work

--- Dungeon_Game_/Assets/Scripts/SkillTree/AbilityHolder.cs
+++ Dungeon_Game_/Assets/Scripts/SkillTree/AbilityHolder.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class AbilityHolder : MonoBehaviour
 {
-
     public Ability ability;
     float cooldownTime;
     float activeTime;
--- Dungeon_Game_/Assets/Scripts/SkillTree/AbilityScripts/Dash.cs
+++ Dungeon_Game_/Assets/Scripts/SkillTree/AbilityScripts/Dash.cs
@@ -2,9 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu]
-
-
 public class Dash : Ability
 {
     public float dashVelocity;
