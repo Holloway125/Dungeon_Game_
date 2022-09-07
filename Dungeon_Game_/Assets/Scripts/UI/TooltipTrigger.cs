ed55746adde607d3d2294dd129779e50268c6e88
Boskazuru <mcmullendalton@yahoo.com>
2022-09-07 09:38:26 -0700

    Tooltip update

--- Dungeon_Game_/Assets/Scripts/UI/TooltipTrigger.cs
+++ Dungeon_Game_/Assets/Scripts/UI/TooltipTrigger.cs
@@ -5,9 +5,14 @@ using UnityEngine.EventSystems;
 
 public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    public string header;
+
+    [Multiline()]
+    public string content;
+
     public void OnPointerEnter(PointerEventData eventData)
     {
-        TooltipSystem.Show();
+        TooltipSystem.Show(content, header);
     }
     
     public void OnPointerExit(PointerEventData eventData)
