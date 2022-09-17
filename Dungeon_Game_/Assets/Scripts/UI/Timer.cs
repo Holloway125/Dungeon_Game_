7c058f19797b968262a19f4df26b68d948f31a76
Boskazuru <mcmullendalton@yahoo.com>
2022-09-16 17:58:27 -0700

    Timer Update

---
+++ Dungeon_Game_/Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 10;
+    public bool timerIsRunning = false;
+    public TextMeshProUGUI timeText;
+
+    private void Start()
+    {
+        timerIsRunning = true;
+    }
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+
+            else if (timeRemaining <= 0)
+            {
+                GameOver();
+            }
+            
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60); 
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    void PauseTimer()
+    {
+        timerIsRunning = false;
+    }
+
+    void StartTimer()
+    {
+        timerIsRunning = true;
+    }
+
+    void GameOver()
+    {
+        SceneManager.LoadScene("LaunchMenu");
+    }
+}
