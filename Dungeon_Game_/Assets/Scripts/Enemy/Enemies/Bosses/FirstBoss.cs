d6e8472d386de8ce010777d36ef84c7d72f0db49
Boskazuru <mcmullendalton@yahoo.com>
2022-09-28 16:47:08 -0700

    Youwin

--- Dungeon_Game_/Assets/Scripts/Enemy/Enemies/Bosses/FirstBoss.cs
+++ Dungeon_Game_/Assets/Scripts/Enemy/Enemies/Bosses/FirstBoss.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class FirstBoss : BaseEnemy
 {
     public override void Death()
     {
-
-        timer.YouWin();
-
+        base.Death();
+        SceneManager.LoadScene("LaunchMenu");
     }        
 }
