d56d703535401e503f6f0ad0252595e0faf124f5
Boskazuru <mcmullendalton@yahoo.com>
2022-10-25 13:11:28 -0700

    Camera

--- Dungeon_Game_/Assets/Scripts/Camera/CameraController.cs
+++ Dungeon_Game_/Assets/Scripts/Camera/CameraController.cs
@@ -7,22 +7,22 @@ public class CameraController : MonoBehaviour
     
     public void MoveUp()
     {
-        transform.position += new Vector3(0, 10, 0);
+        transform.position += new Vector3(0f, 11.25f, 0f);
     }
 
     public void MoveRight()
     {  
-        transform.position += new Vector3(20, 0, 0);
+        transform.position += new Vector3(20f, 0f, 0f);
     }
 
     public void MoveLeft()
     {  
-        transform.position += new Vector3(-20, 0, 0);
+        transform.position += new Vector3(-20f, 0f, 0f);
     }
 
     public void MoveDown()
     {
-        transform.position += new Vector3(0, -10, 0);
+        transform.position += new Vector3(0f, -11.25f, 0f);
     }
 
 
