77116f042cfe9b46e56057a91853223501709b92
Boskazuru <mcmullendalton@yahoo.com>
2022-11-02 17:14:28 -0700

    update

--- Dungeon_Game_/Assets/Scripts/Player/CorePlayer/PlayerController.cs
+++ Dungeon_Game_/Assets/Scripts/Player/CorePlayer/PlayerController.cs
@@ -10,21 +10,16 @@ public class PlayerController : MonoBehaviour
     private PlayerActions _playerActions;
     private Rigidbody2D _rBody;
     private Vector2 _moveInput;
-    private Animator _anim;
 
-    private PlayerInput _playerInput;
-    private InputAction _movement;
+    private Animator _anim;
+    private bool isMoving;
 
     private void Awake()
     {
-        _playerInput = GetComponent<PlayerInput>();
-        _movement = _playerInput.actions["Movement"];
-        // _playerActions = new PlayerActions();
-        // _rBody = GetComponent<Rigidbody2D>();
-        // _anim = GetComponent<Animator>();
-        // if (_rBody is null)
-        //     Debug.LogError("Rigidbody is NULL!");
-        
+        _playerActions = new PlayerActions();
+        _rBody = GetComponent<Rigidbody2D>();
+        if (_rBody is null)
+            Debug.LogError("RigidBody2D is null!");
     }
 
     private void OnEnable()
@@ -37,21 +32,36 @@ public class PlayerController : MonoBehaviour
         _playerActions.Player_Map.Disable();
     }
 
-    void Attacking()
-    {
-        _anim.SetBool("attacking", true);
-        _speed = 0;
-    }
+    // void Attacking()
+    // {
+    //     _anim.SetBool("attacking", true);
+    //     _speed = 0;
+    // }
 
-    void StopAttacking()
-    {
-        _anim.SetBool("attacking", false);
-        _speed = 5;       
-    }
+    // void StopAttacking()
+    // {
+    //     _anim.SetBool("attacking", false);
+    //     _speed = 5;       
+    // }
 
     void FixedUpdate() 
     {
         _moveInput = _playerActions.Player_Map.Movement.ReadValue<Vector2>();
         _rBody.velocity = _moveInput * _speed;
+        _anim.SetFloat("Horizontal",_rBody.velocity.x);
+        _anim.SetFloat("Vertical",_rBody.velocity.y);
+        if(_rBody.velocity.x < 0.1f || _rBody.velocity.x > -0.1f)
+        {
+            isMoving = true;
+        }
+        else if (_rBody.velocity.y < 0.1f || _rBody.velocity.y > -0.1f)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+        _anim.SetBool("isMoving", isMoving);
     }
 }
